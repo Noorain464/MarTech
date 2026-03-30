4ab3d90717b1396bc926b4adb45a78b02a9b20d0
Syeda Noorain <syedanoorain@Syedas-MacBook-Air.local>
2026-03-30 09:55:09 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // const originUrl = (process.env.FRONTEND_URL || 'http://localhost:5173').replace(/\/$/, '');
 
 app.use(cors({
-    origin: "https://mar-tech-teal.vercel.app",
+    origin: ["https://mar-tech-teal.vercel.app", "http://localhost:5173"],
     credentials: true,
 }));
 app.use(express.json());
